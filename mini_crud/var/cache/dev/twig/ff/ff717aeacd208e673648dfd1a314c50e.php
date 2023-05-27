b979e19b22ca46a48416e83147d41ae606938f09
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2023-05-27 19:17:58 -0300

    projeto criado, foi adicionado o Bootstrap 5, criado o Controller Home

---
+++ mini_crud/var/cache/dev/twig/ff/ff717aeacd208e673648dfd1a314c50e.php
@@ -0,0 +1,219 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* home/index.html.twig */
+class __TwigTemplate_fe19c9f495d20f076c3a801e1d41d6f0 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "home/index.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "home/index.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "home/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        // line 4
+        echo "Hello HomeController!
+
+
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 9
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 10
+        echo "
+<div class=\"container\">
+    <nav class=\"navbar navbar-expand-lg bg-body-tertiary\">
+        <div class=\"container-fluid\">
+            <a class=\"navbar-brand\" href=\"#\">Navbar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\"
+                data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\"
+                aria-label=\"Toggle navigation\">
+                <span class=\"navbar-toggler-icon\"></span>
+            </button>
+            <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+                <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"#\">Link</a>
+                    </li>
+
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link disabled\">Disabled</a>
+                    </li>
+                </ul>
+                <form class=\"d-flex\" role=\"search\">
+                    <input class=\"form-control me-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">
+                    <button class=\"btn btn-outline-success\" type=\"submit\">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <br>
+    <form>
+        <div class=\"mb-0\">
+          <label for=\"exampleInputEmail1\" class=\"form-label\">Email address</label>
+          <input type=\"email\" class=\"form-control\" id=\"exampleInputEmail1\" aria-describedby=\"emailHelp\">
+        </div>
+        <div class=\"mb-0\">
+          <label for=\"exampleInputPassword1\" class=\"form-label\">Password</label>
+          <input type=\"password\" class=\"form-control\" id=\"exampleInputPassword1\">
+        </div>
+        <button type=\"submit\" class=\"btn btn-primary\">Submit</button>
+      </form>
+
+</div>
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "home/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  92 => 10,  82 => 9,  69 => 4,  59 => 3,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %}
+Hello HomeController!
+
+
+{% endblock %}
+
+{% block body %}
+
+<div class=\"container\">
+    <nav class=\"navbar navbar-expand-lg bg-body-tertiary\">
+        <div class=\"container-fluid\">
+            <a class=\"navbar-brand\" href=\"#\">Navbar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\"
+                data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\"
+                aria-label=\"Toggle navigation\">
+                <span class=\"navbar-toggler-icon\"></span>
+            </button>
+            <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+                <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"#\">Link</a>
+                    </li>
+
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link disabled\">Disabled</a>
+                    </li>
+                </ul>
+                <form class=\"d-flex\" role=\"search\">
+                    <input class=\"form-control me-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">
+                    <button class=\"btn btn-outline-success\" type=\"submit\">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <br>
+    <form>
+        <div class=\"mb-0\">
+          <label for=\"exampleInputEmail1\" class=\"form-label\">Email address</label>
+          <input type=\"email\" class=\"form-control\" id=\"exampleInputEmail1\" aria-describedby=\"emailHelp\">
+        </div>
+        <div class=\"mb-0\">
+          <label for=\"exampleInputPassword1\" class=\"form-label\">Password</label>
+          <input type=\"password\" class=\"form-control\" id=\"exampleInputPassword1\">
+        </div>
+        <button type=\"submit\" class=\"btn btn-primary\">Submit</button>
+      </form>
+
+</div>
+{% endblock %}", "home/index.html.twig", "/Users/abrahaoeneias/Documents/Dev/Symfony/mini_crud/templates/home/index.html.twig");
+    }
+}
